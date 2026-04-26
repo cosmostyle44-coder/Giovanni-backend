d33d548881bbe4fa538ad9b0c3a5b620242394ab
cosmostyle44-coder <cosmostyle44@gmail.com>
2026-04-27 01:32:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -83,4 +83,4 @@ async function sendEbook(email, name) {
 }
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Serveur lancé sur le port ' + PORT));
+app.listen(PORT, '0.0.0.0', () => console.log('Serveur lancé sur le port ' + PORT));
